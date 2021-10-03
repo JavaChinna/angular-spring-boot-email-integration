fabce032e212d7f3b6eaad3e01586297769adbf6
Chinna <inbox4chinna@gmail.com>
2021-10-03 19:28:45 +0530

    Initial commit

---
+++ spring-boot-oauth2-social-login/src/main/java/com/javachinna/config/AppConstants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.javachinna.config;
+
+/**
+ * @author Chinna
+ *
+ */
+public class AppConstants {
+	public static final String TOKEN_INVALID = "INVALID";
+	public static final String TOKEN_EXPIRED = "EXPIRED";
+	public static final String TOKEN_VALID = "VALID";
+	public final static String SUCCESS = "success";
+}
